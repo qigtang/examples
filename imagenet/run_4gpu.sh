5a13ae39b981c39b258fb94d451e0623c9589655
Christian Sarofeen <csarofeen@nvidia.com>
2017-12-15 22:01:35 +0000

    Cleanup run script.

--- imagenet/run_4gpu.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-for GPU in `seq 0 2`; do python main.py -j 10 --arch resnet50 --b 128 --world-size 4 --gpu $GPU --dist-backend nccl --fp16 . > ${GPU}.log & done ; python main.py --arch resnet50 --b 128 --world-size 4 --gpu 3 -j 10 --dist-backend nccl --fp16 .
